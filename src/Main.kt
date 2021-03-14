215bbfecfbb3a53d8afd8836354983a55f3825bc
krylovam <krylova.maria.2629@gmail.com>
2021-03-14 14:56:02 +0300

    update solution

--- src/Main.kt
+++ src/Main.kt
@@ -7,8 +7,8 @@ fun main() {
     val minLength = 8
     val fileName = "words.txt"
     val userWordsFileName = "user_words.txt"
-    var wordSet = mutableSetOf<String>()
-    var longWordsList = mutableListOf<String>()
+    val wordSet = mutableSetOf<String>()
+    val longWordsList = mutableListOf<String>()
     runBlocking {
         File(fileName).useLines { lines -> wordSet.addAll(lines) }
         for (word in wordSet) {
@@ -17,30 +17,31 @@ fun main() {
             }
         }
 
-        val randWord = longWordsList.get(abs(Random.nextInt()) % longWordsList.size)
-        println("\"$randWord\"\nСоставьте из букв слова как можно больше слов через пробел")
-        println("Когда закончите, нажмите Enter")
-        var letterMap = mutableMapOf<Char, Int>()
+        val randWord = longWordsList[abs(Random.nextInt()) % longWordsList.size]
+        println("Составьте из букв слова \"$randWord\" как можно больше слов " +
+                "\nСлова вписывать через пробел" +
+                "\nПо окончании нажмите Enter")
+        val letterMap = mutableMapOf<Char, Int>()
         for (letter in randWord) {
             if (letterMap[letter] != null) {
-                letterMap.put(letter, letterMap[letter]!! + 1)
+                letterMap[letter] = letterMap[letter]!! + 1
             } else {
-                letterMap.put(letter, 1)
+                letterMap[letter] = 1
             }
         }
         val userLine = readLine()
         if (userLine != null) {
             val userWords = userLine.split(" ").toTypedArray()
-            var correctUserWords = mutableListOf<String>()
+            val correctUserWords = mutableListOf<String>()
             for (word in userWords) {
-                var currLetterMap = letterMap.toMutableMap()
+                val currLetterMap = letterMap.toMutableMap()
                 var isCorrectWord = true
                 for (letter in word) {
                     if (currLetterMap[letter] === null) {
                         isCorrectWord = false
                         break
                     }
-                    currLetterMap.put(letter, currLetterMap[letter]!! - 1)
+                    currLetterMap[letter] = currLetterMap[letter]!! - 1
                     if (currLetterMap[letter]!! < 0) {
                         isCorrectWord = false
                         break
